2060138f8aa3ff5d83faa81d816d19ed65dd8735
coder0227 <coder0227@gmail.com>
2024-05-14 21:42:02 +0500

    Add project files.

---
+++ src/PersonalBrand.UI/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PersonalBrand.Domain.Entities.Auth;
+using PersonalBrand.Domain.IdentityModels;
+
+namespace PersonalBrand.UI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController(UserManager<UserModel> userManager) : ControllerBase
+    {
+        private readonly UserManager<UserModel> _userManager = userManager;
+
+        [HttpPost]
+        public async Task<IActionResult> Register(Register registerModel)
+        {
+            return Ok(registerModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Login loginModel)
+        {
+            return Ok(loginModel);
+        }
+    }
+}
